cc91089517a14df67a7e687cba724fb9d1593220
dcod3i <pvnck1995@gmail.com>
2024-04-23 10:33:30 +0530

    added user, layout, markattendance files

--- app/(home)/markattendance.js
+++ app/(home)/markattendance.js
@@ -1,14 +1,177 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import axios from "axios";
+import { AntDesign } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 const markattendance = () => {
+  const router = useRouter();
+  const [currentDate, setCurrentDate] = useState(moment());
+
+  const goToNextDay = () => {
+    const nextDate = moment(currentDate).add(1, "days");
+    setCurrentDate(nextDate);
+  };
+
+  const goToPrevDay = () => {
+    const prevDate = moment(currentDate).subtract(1, "days");
+    setCurrentDate(prevDate);
+  };
+
+  const formatDate = (date) => {
+    return date.format("MMMM D, YYYY");
+  };
+
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    const fetchEmployeeData = async () => {
+      try {
+        const response = await axios.get("http://localhost:9000/employees");
+        setEmployees(response.data);
+      } catch (error) {
+        console.log("Error fetching employee data", error);
+      }
+    };
+    fetchEmployeeData();
+  }, []);
+
+  const [attendance, setAttendance] = useState([]);
+  const fetchAttendanceData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:9000/attendance`, {
+        params: {
+          date: currentDate.format("MMMM D, YYYY"),
+        },
+      });
+      setAttendance(response.data);
+    } catch (error) {
+      console.log("error fetching attendance data", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAttendanceData();
+  }, [currentDate]);
+
+  
+  const employeeWithAttendance = employees.map((employee) => {
+    const attendanceRecord = attendance.find(
+      (record) => record.employeeId === employee.employeeId
+    );
+
+    return {
+      ...employee,
+      status: attendanceRecord ? attendanceRecord.status : "",
+    };
+  });
+  console.log(employeeWithAttendance);
+
   return (
-    <View>
-      <Text>markattendance</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Pressable>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 10,
+            marginRight: "auto",
+            marginLeft: "auto",
+            marginVertical: 20,
+          }}
+        >
+          <AntDesign
+            onPress={goToPrevDay}
+            name="left"
+            size={24}
+            color="black"
+          />
+          <Text>{formatDate(currentDate)}</Text>
+          <AntDesign
+            onPress={goToNextDay}
+            name="right"
+            size={24}
+            color="black"
+          />
+        </View>
+
+        <View style={{ marginHorizontal: 12 }}>
+          {employeeWithAttendance.map((item, index) => {
+            console.log("item.status:", item.status);
+            console.log("item.status.charAt(0):", item.status.charAt(0));
+            
+            return (
+              <Pressable
+                onPress={() =>
+                  router.push({
+                    pathname: "/[user]",
+                    params: {
+                      name: item.employeeName,
+                      id: item.employeeId,
+                      salary: item?.salary,
+                      designation: item?.designation,
+                    },
+                  })
+                }
+                style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+              >
+                <View
+                  style={{
+                    width: 50,
+                    height: 50,
+                    borderRadius: 8,
+                    padding: 10,
+                    backgroundColor: "#4b6cb7",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginTop: 5,
+                  }}
+                >
+                  <Text style={{ color: "white", fontSize: 16 }}>
+                    {item?.employeeName?.charAt(0)}
+                  </Text>
+                </View>
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                    {item?.employeeName}
+                  </Text>
+                  <Text style={{ marginTop: 5, color: "gray" }}>
+                    {item?.designation} ({item?.employeeId})
+                  </Text>
+                </View>
+                {item?.status && (
+                  <View
+                    style={{
+                      width: 50,
+                      height: 50,
+                      borderRadius: 8,
+                      padding: 10,
+                      backgroundColor: "#FF69B4",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {item.status.charAt(0)}
+                    </Text>
+                  </View>
+                )}
+              </Pressable>
+            );
+          })}
+        </View>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default markattendance
+export default markattendance;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
